6e1c85b16fbca7e165bfc95d5986db25516fc132
Johan Edstedt <edstedt.johan@gmail.com>
2026-04-02 01:07:11 +0200

    no swiglu

--- src/loma/descriptor/transformer/layers/__init__.py
+++ src/loma/descriptor/transformer/layers/__init__.py
@@ -6,6 +6,6 @@
 
 from .mlp import Mlp
 from .patch_embed import PatchEmbed
-from .swiglu_ffn import SwiGLUFFN
+# from .swiglu_ffn import SwiGLUFFN
 from .block import Block
 from .attention import Attention
